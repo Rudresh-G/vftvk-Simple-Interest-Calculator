dc0651de237ff619d99768296421d0db37233717
Rudresh <rudresh.c2@gmail.com>
2022-05-01 06:09:49 +0530

    final code

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    A = p*(1 + r*t/100)-p;
+    currYear = parseInt(new Date().getFullYear(), 10);
+    targetYear = (currYear + parseInt(t));
+    printText = "If you deposit <mark>" + p + "</mark> , <br> at an interest rate of <mark>" + r + "%</mark>.<br> You will recieve an amount of <mark>" + A + "</mark>,<br> in the year <mark>" + targetYear;
+    document.getElementById("result").innerHTML=printText
     
+}
+
+function show_val(x)
+{
+    document.getElementById("rateDisp").innerHTML=x +"%"
 }
         
